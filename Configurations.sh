d0afea855f484f0f94d2a22a79e8931afa095b25
Ali Asghari <125490977+AliAsghariparand@users.noreply.github.com>
2024-04-09 08:21:34 +0330

    Configurations Script

--- Configurations.sh
+++ Configurations.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/root/sing-box run -c /root/Hysteria_Udp_Tls_Dns.json & /root/sing-box run -c /root/Vless_Tcp_Tls_Dns.json & /root/sing-box run -c /root/Shadowsocks_Tcp_Dns.json s
+/root/sing-box run -c /root/Hysteria_Udp_Tls_Dns.json & /root/sing-box run -c /root/Vless_Tcp_Tls_Dns.json & /root/sing-box run -c /root/Shadowsocks_Tcp_Dns.json
